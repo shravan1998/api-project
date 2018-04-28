6c0b045c9a4f43dc24e778ff078331163fdab0f8
Afroz Hussain <36099616+coderhawk999@users.noreply.github.com>
2018-04-29 00:02:13 +0530

    Update api.js

--- js/api.js
+++ js/api.js
@@ -1,22 +1,21 @@
+var weather,description;
+var api='http://api.openweathermap.org/data/2.5/weather?q=';
+var id='&APPID=e14b4cb410b59c24981c46fe3e32bfc7';
+var units='&units=metric';
+var city;
+var url=api+city+id+units;;
 
-document.getElementById("search").onclick=function(){currentweather()};
-function currentweather(){
-    var city=document.getElementById("input").value;
-    var weather,description;
-    var api='http://api.openweathermap.org/data/2.5/weather?q=';
-    var id='&APPID=e14b4cb410b59c24981c46fe3e32bfc7';
-    var units='&units=metric';
-    var url=api+city+id+units;
-        fetch(url)
-        .then(  
+function load(){
+    city = document.getElementById("search").value;
+fetch(url)
+.then(  
 		    function(response) {  
                 return response.json();
             })
             .then(function(data){
                
-                document.getElementById("temp").innerHTML=data.main.temp;
-                document.getElementById("max_temp").innerHTML=data.main.temp_max;
-                document.getElementById("min_temp").innerHTML=data.main.temp_min;
-                document.getElementById("humid").innerHTML=data.main.humidity;
+                console.log(JSON.stringify(data));
+                console.log(data["main"]["temp"]);
+                
             });
-        } 
+}
