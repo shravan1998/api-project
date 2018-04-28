e4405218e22e78b4c642946d04e31d05fc4ad213
Afroz Hussain <36099616+coderhawk999@users.noreply.github.com>
2018-04-29 00:23:02 +0530

    Update api.js

--- js/api.js
+++ js/api.js
@@ -2,7 +2,7 @@ document.getElementById("search").onclick=function(){currentweather()};
 function currentweather(){
     var city=document.getElementById("input").value;
     var weather,description;
-    var api='http://api.openweathermap.org/data/2.5/weather?q=';
+    var api='https://api.openweathermap.org/data/2.5/weather?q=';
     var id='&APPID=e14b4cb410b59c24981c46fe3e32bfc7';
     var units='&units=metric';
     var url=api+city+id+units;
