ce6f97316f74cc8246b27d360ba722980a749aab
shravan1998 <shrkum51@gmail.com>
2018-05-01 23:54:17 +0530

    Done

--- js/api.js
+++ js/api.js
@@ -1,22 +1,46 @@
-document.getElementById("search").onclick=function(){currentweather()};
+document.getElementById("search").onclick=function(){currentweatherclick()};
 document.getElementById("input").addEventListener("keyup",currentweather);
-function currentweather(){
-    var city=document.getElementById("input").value;
-    var weather,description;
-    var api='https://api.openweathermap.org/data/2.5/weather?q=';
-    var id='&APPID=e14b4cb410b59c24981c46fe3e32bfc7';
-    var units='&units=metric';
-    var url=api+city+id+units;
-        fetch(url)
-        .then(  
-		    function(response) {  
-                return response.json();
-            })
-            .then(function(data){
-               
-                document.getElementById("temp").innerHTML=data.main.temp;
-                document.getElementById("max_temp").innerHTML=data.main.temp_max;
-                document.getElementById("min_temp").innerHTML=data.main.temp_min;
-                document.getElementById("humid").innerHTML=data.main.humidity;
-            });
+function currentweather(event){
+        var city=document.getElementById("input").value;
+        var weather,description;
+        var api='https://api.openweathermap.org/data/2.5/weather?q=';
+        var id='&APPID=e14b4cb410b59c24981c46fe3e32bfc7';
+        var units='&units=metric';
+        var url=api+city+id+units;
+        if(event.keyCode == 13){
+            fetch(url)
+            .then(  
+                function(response) {  
+                    return response.json();
+                })
+                .then(function(data){
+                
+                    document.getElementById("temp").innerHTML=data.main.temp;
+                    document.getElementById("max_temp").innerHTML=data.main.temp_max;
+                    document.getElementById("min_temp").innerHTML=data.main.temp_min;
+                    document.getElementById("humid").innerHTML=data.main.humidity;
+                });
+            }
         } 
+        function currentweatherclick(){
+            var city=document.getElementById("input").value;
+            var weather,description;
+            var api='https://api.openweathermap.org/data/2.5/weather?q=';
+            var id='&APPID=e14b4cb410b59c24981c46fe3e32bfc7';
+            var units='&units=metric';
+            var url=api+city+id+units;
+            
+                fetch(url)
+                .then(  
+                    function(response) {  
+                        return response.json();
+                    })
+                    .then(function(data){
+                    
+                        document.getElementById("temp").innerHTML=data.main.temp;
+                        document.getElementById("max_temp").innerHTML=data.main.temp_max;
+                        document.getElementById("min_temp").innerHTML=data.main.temp_min;
+                        document.getElementById("humid").innerHTML=data.main.humidity;
+                    });
+                
+            } 
